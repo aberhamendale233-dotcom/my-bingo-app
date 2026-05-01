fdff8c20dad51de776fb0b51249b0496a4fa7f4e
aberhamendale233-dotcom <aberhamendale233@gmail.com>
2026-05-01 17:43:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 const express = require('express');
-const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
-
 const app = express();
 const PORT = 3000;
 
-// ዳታቤዙን ማገናኘት
-const db = new sqlite3.Database('./bingo.db', (err) => {
-    if (err) console.error("Database connection error:", err.message);
-    else console.log("Connected to bingo.db database.");
-});
-
-app.use(express.json());
 app.use(express.static(__dirname));
 
-// የገንዘብ መጠን ለማየት
-app.get('/api/balance/:userId', (req, res) => {
-    const userId = req.params.userId;
-    db.get("SELECT balance FROM users WHERE id = ?", [userId], (err, row) => {
-        if (row) res.json({ balance: row.balance });
-        else res.json({ balance: 0 });
-    });
+app.get('/', (req, res) => {
+    res.send("ሰርቨሩ በትክክል እየሰራ ነው!");
 });
 
-// ሰርቨሩን ማስነሳት
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
