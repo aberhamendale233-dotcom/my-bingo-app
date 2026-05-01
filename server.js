fbe7893ef03c1afa922783a4e781df8b4e362037
aberhamendale233-dotcom <aberhamendale233@gmail.com>
2026-05-01 15:46:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// ፋይሎቹ የሚገኙበትን ፎልደር ማሳወቅ
+app.use(express.static(path.join(__dirname)));
+
+// ዋናውን ገጽ (index.html) መክፈት
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// ሰርቨሩን ማስነሳት
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
